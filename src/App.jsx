34c6523e28a2074718077c55bb5a46091a48cf6b
SrtaLiv <olitutos712@gmail.com>
2024-12-17 13:06:52 -0300

    Configuracion base lista

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl text-pink-300 font-bold underline">
-      Helloo world!
-    </h1>
+      <Canvas
+        camera={{
+          position: [3, 3, 3]
+        }}>
+        <color attach="background" args={["#333333"]} />
+        <OrbitControls />
+        <mesh>
+          <boxGeometry args={[0.5, 0.5, 0.5]} />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas >
+
     </>
   )
 }
